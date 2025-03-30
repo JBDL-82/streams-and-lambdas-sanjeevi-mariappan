76e8b1dc248c1284923184fe8d2602f7c56a5623
sanjeevi-mariappan <45958244+sanjeevi-mariappan@users.noreply.github.com>
2025-03-30 12:51:40 +0530

    Delete src directory

--- src/DepartmentsWithNoEmployees.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class DepartmentsWithNoEmployees {
-    
-    public static void main(String[] args) {
-        List<Employee> listOfEmployee = Employee.getAllEmployees();
-        listOfEmployee.stream().filter(employee -> employee.getName().trim().equals("")).map(Employee::getDepartment).forEach(System.out::println);
-    }
-}
